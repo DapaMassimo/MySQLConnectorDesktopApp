f9c995e33f51e288428694a8b202bd105d72a50d
DapaMassimo <massimo.dapa96@gmail.com>
2022-09-17 14:35:40 +0200

    Changed app startup.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from mysql_connector_app import app
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     app.start()
--- mysql_connector_app/app.py
+++ mysql_connector_app/app.py
@@ -1,12 +1,12 @@
 from . import controller
 
+def start():
+    app = controller.Application('MySQL connector')
 
-app = controller.Application('MySQL connector')
+    app.attributes('-topmost', True)
+    app.update()
+    app.attributes('-topmost', False)
 
-app.attributes('-topmost', True)
-app.update()
-app.attributes('-topmost', False)
-
-app.eval('tk::PlaceWindow . top')
-app.mainloop()
-app.close_connection()
+    app.eval('tk::PlaceWindow . top')
+    app.mainloop()
+    app.close_connection()
